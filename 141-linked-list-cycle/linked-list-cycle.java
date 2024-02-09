ed64b98621beb5f5e7f3df83966a7549a41ed10b
JeonB <newjeonb@gmail.com>
2024-02-09 14:03:04 +0900

    Time: 4 ms (17.40%) | Memory: 44.3 MB (50.08%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> visited_nodes = new HashSet<>();
+        ListNode current_node = head;
+        while (current_node != null) {
+            if (visited_nodes.contains(current_node)) {
+                return true;
+            }
+            visited_nodes.add(current_node);
+            current_node = current_node.next;
+        }
+        return false;
+    }
+}
