9df94785f7040581b853f578bc154787be474382
JeonB <newjeonb@gmail.com>
2024-02-11 15:10:12 +0900

    Time: 4 ms (17.23%) | Memory: 44.4 MB (40.31%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,14 +11,26 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode slow_pointer = head, fast_pointer = head;
-        while (fast_pointer != null && fast_pointer.next != null) {
-            slow_pointer = slow_pointer.next;
-            fast_pointer = fast_pointer.next.next;
-            if (slow_pointer == fast_pointer) {
+        // ListNode slow_pointer = head, fast_pointer = head;
+        // while (fast_pointer != null && fast_pointer.next != null) {
+        //     slow_pointer = slow_pointer.next;
+        //     fast_pointer = fast_pointer.next.next;
+        //     if (slow_pointer == fast_pointer) {
+        //         return true;
+        //     }
+        // }
+        // return false;
+        HashSet<ListNode> visited_nodes = new HashSet<>();
+        ListNode current_node = head;
+        while(current_node != null){
+            if(visited_nodes.contains(current_node)){
                 return true;
             }
+            visited_nodes.add(current_node);
+            current_node = current_node.next;
         }
         return false;
+        
     }
 }
+
