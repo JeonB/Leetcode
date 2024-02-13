54007a32590a7dd88abb31c3d8c54e26d86a1976
JeonB <newjeonb@gmail.com>
2024-02-13 23:29:15 +0900

    Time: 0 ms (100.00%) | Memory: 40.7 MB (8.02%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,22 @@
+class Solution {
+    int[] arr;
+    public int climbStairs(int n) {
+        //한번에 한 계단 혹은 두 계단만 오를 수 있는 n개의 계단 오르는 경우의 수
+        arr = new int[n+1];
+        
+        if(n<3)
+            return n;
+        
+        return pivonazi(n);
+    }
+
+    public int pivonazi(int n){
+        arr[1] = 1;
+        arr[2] = 2;
+        
+        for(int i = 3 ; i<n+1; i++){
+            arr[i] = arr[i-1]+arr[i-2];
+        }
+        return arr[n];
+    }
+}
