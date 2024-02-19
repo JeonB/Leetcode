87202413f73696e640beb56592694797ac4608ff
JeonB <newjeonb@gmail.com>
2024-02-19 20:58:14 +0900

    Time: 5 ms (67.51%) | Memory: 42.2 MB (20.51%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.java
+++ 409-longest-palindrome/longest-palindrome.java
@@ -1,28 +1,12 @@
 class Solution {
     public int longestPalindrome(String s) {
-        int[] charCounts = new int[128]; // ASCII 문자 범위
-        
-        // 문자열에서 각 문자의 출현 빈도를 카운팅
+        Set<Character> set = new HashSet<>();
         for (char c : s.toCharArray()) {
-            charCounts[c]++;
+            if (set.contains(c)) set.remove(c);
+            else set.add(c);
         }
-        
-        int length = 0;
-        boolean hasOddCount = false;
-        
-        // 카운트된 문자들로부터 팰린드롬 길이 계산
-        for (int count : charCounts) {
-            length += count / 2 * 2; // 각 문자의 출현 빈도 중 짝수 개를 팰린드롬 길이에 추가
-            if (count % 2 == 1) { // 홀수 개의 문자가 있다면
-                hasOddCount = true; // 홀수 개의 문자가 있음을 표시
-            }
-        }
-        
-        // 홀수 개의 문자가 있을 경우 가운데 하나를 추가하여 팰린드롬 길이를 홀수로 만듦
-        if (hasOddCount) {
-            length++;
-        }
-        
-        return length;
+
+        int odd = set.size();
+        return s.length() - (odd == 0 ? 0 : odd - 1);
     }
 }
