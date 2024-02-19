cfab7c0945dbc0260d1e74a3741bc2978b3e6d94
JeonB <newjeonb@gmail.com>
2024-02-19 20:49:48 +0900

    Time: 5 ms (67.51%) | Memory: 41.7 MB (70.41%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        
+        if(s.length() == 1)
+            return 1;
+        char[] charArray = s.toCharArray();
+        Arrays.sort(charArray);
+        String sortedStr = new String(charArray);
+        int cnt = 1;
+        int ans = 0;
+        for(int i = 0 ; i<s.length()-1 ; i++){
+            
+            if(sortedStr.charAt(i) == sortedStr.charAt(i+1))
+                cnt++;   
+            else{
+                if(cnt % 2 == 0){
+                    ans+=cnt;
+                }
+                else{
+                    ans+=(cnt-1);
+                    }
+                cnt = 1;  
+            }
+        }
+      
+        ans+=cnt;
+        
+        if(ans % 2 == 0 && ans<s.length())
+            return ++ans;
+        return ans;
+
+    }
+}
