48005f959b2827e26fd934ca009fe4464ec41b01
JeonB <newjeonb@gmail.com>
2024-02-19 20:51:41 +0900

    Time: 1 ms (100.00%) | Memory: 42.1 MB (20.51%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.java
+++ 409-longest-palindrome/longest-palindrome.java
@@ -1,33 +1,28 @@
 class Solution {
     public int longestPalindrome(String s) {
+        int[] charCounts = new int[128]; // ASCII 문자 범위
         
-        if(s.length() == 1)
-            return 1;
-        char[] charArray = s.toCharArray();
-        Arrays.sort(charArray);
-        String sortedStr = new String(charArray);
-        int cnt = 1;
-        int ans = 0;
-        for(int i = 0 ; i<s.length()-1 ; i++){
-            
-            if(sortedStr.charAt(i) == sortedStr.charAt(i+1))
-                cnt++;   
-            else{
-                if(cnt % 2 == 0){
-                    ans+=cnt;
-                }
-                else{
-                    ans+=(cnt-1);
-                    }
-                cnt = 1;  
+        // 문자열에서 각 문자의 출현 빈도를 카운팅
+        for (char c : s.toCharArray()) {
+            charCounts[c]++;
+        }
+        
+        int length = 0;
+        boolean hasOddCount = false;
+        
+        // 카운트된 문자들로부터 팰린드롬 길이 계산
+        for (int count : charCounts) {
+            length += count / 2 * 2; // 각 문자의 출현 빈도 중 짝수 개를 팰린드롬 길이에 추가
+            if (count % 2 == 1) { // 홀수 개의 문자가 있다면
+                hasOddCount = true; // 홀수 개의 문자가 있음을 표시
             }
         }
-      
-        ans+=cnt;
         
-        if(ans % 2 == 0 && ans<s.length())
-            return ++ans;
-        return ans;
-
+        // 홀수 개의 문자가 있을 경우 가운데 하나를 추가하여 팰린드롬 길이를 홀수로 만듦
+        if (hasOddCount) {
+            length++;
+        }
+        
+        return length;
     }
 }
