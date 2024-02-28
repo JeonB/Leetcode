085aa972a209d708277919bccb20adabf6bee42d
JeonB <newjeonb@gmail.com>
2024-02-28 09:09:15 +0900

    Time: 2 ms (97.64%) | Memory: 55.8 MB (57.88%) - LeetSync

--- 890-lemonade-change/lemonade-change.java
+++ 890-lemonade-change/lemonade-change.java
@@ -1,7 +1,8 @@
 class Solution {
-    // 손님은 오직 하나의 레몬에이드만 구매함. 5달러
-    // 잔돈 없는 상태로 시작
-      public boolean lemonadeChange(int[] bills) {
+    public boolean lemonadeChange(int[] bills) {
+        if(bills[0]>5)
+            return false;
+            
         int five = 0, ten = 0;
         for (int i : bills) {
             if (i == 5) five++;
