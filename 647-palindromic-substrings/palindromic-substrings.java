a42b9a10155ec352cbf05e87a0329fc16d5bfa4d
JeonB <newjeonb@gmail.com>
2024-03-13 13:43:43 +0900

    Time: 18 ms (37.90%) | Memory: 48.5 MB (5.25%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,29 +1,33 @@
 class Solution {
     public int countSubstrings(String s) {
-        int n = s.length();
-        boolean[][] palindrome= new boolean[n][n];
-        int ans = 0;
+        int[][] subPalin = new int[s.length()][s.length()];
+        int ans = 1;
+        subPalin[s.length() - 1][s.length() - 1] = 1;
 
-        for(int i = 0 ; i<n;i++){
-            palindrome[i][i] = true;
-            ans++;
-        }
-        for(int i = 0 ; i<n-1;i++){
-            if(s.charAt(i) == s.charAt(i+1)) {
-                palindrome[i][i+1] = true;
-                ans++;
+        for(int i = 0 ; i< s.length() - 1 ; i++){
+            subPalin[i][i] = 1;
+            ans+=1;
+            if(s.charAt(i)  == s.charAt(i+1)){
+                subPalin[i][i + 1] = 1;
+                ans+=1;
             }
         }
+        
+        if(s.length() > 2){
+            
+            for(int len = 2 ; len < s.length() ; len++){
 
-        for(int len=3; len<= n ; len++){
-            for(int i =0; i<n-len+1; i++){
-                if(s.charAt(i) == s.charAt(i+len-1) && palindrome[i+1][i+len-2]){
-                    palindrome[i][i+len-1] = true;
-                    ans++;
+                for(int i = 0 ; (i+len)< s.length() ; i++ ){
+                    if(s.charAt(i) == s.charAt(i+len)){
+                        subPalin[i][i+len] = subPalin[i+1][i+len-1];
+                        ans += subPalin[i][i+len]; // subPalin[1][1]
+                    }
                 }
+                
             }
+
         }
+        
         return ans;
     }
-    
 }
