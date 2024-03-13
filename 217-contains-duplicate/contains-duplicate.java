4986f741537f35dfe5f2ffcd279850c2d9af9823
JeonB <newjeonb@gmail.com>
2024-03-13 21:11:38 +0900

    Time: 21 ms (10.38%) | Memory: 55.3 MB (82.67%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        boolean ans = false;
+        Arrays.sort(nums);
+
+        for(int i = 0; i<nums.length - 1 ; i++){
+            if(nums[i] == nums[i+1]){
+                ans = true;
+                break;
+            }
+
+        }
+
+        return ans;
+    }
+}
