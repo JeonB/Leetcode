a940112774c9a9f8f6b11463e9078667da48bbd7
JeonB <newjeonb@gmail.com>
2024-03-19 19:46:15 +0900

    Time: 0 ms (100.00%) | Memory: 40.1 MB (68.54%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,20 +1,24 @@
 class Solution {
+    int[] arr;
     public int climbStairs(int n) {
-        // n =1 > 1, n=2 > 2 , n=3 > 3, n=4 > 5
-        if(n == 1)
-            return 1;
-        else if(n==2)
-            return 2;
-        else{
-            int ans = 0;
-            int prevValue = 2;
-            int prevValue2 = 1;
-            for(int i = 2 ; i<n;i++){
-                ans = prevValue + prevValue2;
-                prevValue2 = prevValue;
-                prevValue = ans;
-            }
-            return ans;
+        if(n<3)
+            return n;
+            
+        //한번에 한 계단 혹은 두 계단만 오를 수 있는 n개의 계단 오르는 경우의 수
+        arr = new int[n+1];
+        
+        
+        
+        return pivonazi(n);
+    }
+
+    public int pivonazi(int n){
+        arr[1] = 1;
+        arr[2] = 2;
+        
+        for(int i = 3 ; i<n+1; i++){
+            arr[i] = arr[i-1]+arr[i-2];
         }
+        return arr[n];
     }
 }
