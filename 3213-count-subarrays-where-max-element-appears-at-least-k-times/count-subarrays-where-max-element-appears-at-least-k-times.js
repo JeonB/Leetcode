879faa4a88ef2a12dcec55703d47cc1e40440283
JeonB <newjeonb@gmail.com>
2024-03-29 21:21:48 +0900

    Time: 90 ms (85.71%) | Memory: 62.3 MB (50.00%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.js
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.js
@@ -8,7 +8,7 @@ var countSubarrays = function(nums, k) {
     const max = Math.max(...nums);
     for (let l = 0, r = 0; r < nums.length; r++) {
         if (nums[r] === max) count++;
-        while (count == k) {
+        while (count === k) {
             subarrays += nums.length - r;
             if (nums[l++] === max) count--;
         }
