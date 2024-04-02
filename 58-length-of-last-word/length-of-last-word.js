ec26e1632600da0df15b211c779e8102cd93672b
JeonB <newjeonb@gmail.com>
2024-04-02 22:08:54 +0900

    Time: 38 ms (98.27%) | Memory: 48.6 MB (81.41%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -3,15 +3,11 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    let nonspaceString = s.trim();
-    let ans = 0;
-    
-    for(let i = nonspaceString.length-1 ; i >= 0 ;i--){
-        if(nonspaceString.charAt(i).includes(' '))
-            return ans;
-        
-        ans++;
+    let len = 0 , tail = s.length - 1;
+    while(tail >= 0 && s[tail] === ' ') tail--;
+    while(tail >= 0 && s[tail] !== ' ') {
+        len++;
+        tail--;
     }
-
-    return ans;
+    return len;
 };
