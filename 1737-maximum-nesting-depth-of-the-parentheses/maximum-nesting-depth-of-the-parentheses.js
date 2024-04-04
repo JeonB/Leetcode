441f607ff4ef70f30e418f661a274139ab9910ea
JeonB <newjeonb@gmail.com>
2024-04-04 19:46:08 +0900

    Time: 50 ms (73.04%) | Memory: 49.2 MB (20.58%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.js
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.js
@@ -3,13 +3,16 @@
  * @return {number}
  */
 var maxDepth = function(s) {
-    let ans = 0,openParen=0;
-    for(let i = 0 ; i<s.length;i++){
-        if(s.charAt(i)==='('){
-            openParen++;
-            ans = Math.max(ans, openParen) }
-        else if(s.charAt(i)===')')
-            openParen--;
+    let a=[];
+    let n=0;
+    for(let i=0;i<s.length;i++){
+        if(s[i]==='('){
+            a.push(s[i]);
+            n=n>a.length?n:a.length;
+        }
+        else if(s[i]===')'){
+            a.pop();
+        }
     }
-    return ans;
+    return n;
 };
