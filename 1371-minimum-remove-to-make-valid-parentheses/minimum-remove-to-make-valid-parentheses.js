9ed749c0a30eab03ca28dfa1e56465b6ebc934ec
JeonB <newjeonb@gmail.com>
2024-04-07 21:52:20 +0900

    Time: 70 ms (81.72%) | Memory: 56.1 MB (89.93%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.js
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.js
@@ -3,40 +3,37 @@
  * @return {string}
  */
 var minRemoveToMakeValid = function(s) {
-    let str = '';
-    const stack = [];
+   const stack = [];
+    const sArray = s.split('');
 
-    // 문자열을 순회하며 괄호 유효성 검사 및 필요한 괄호 제거
-    for (let char of s) {
-        if (char === '(') {
-            stack.push(char);
-        } else if (char === ')') {
-            if (stack.length > 0) {
-                stack.pop();
+    for (let i = 0; i < sArray.length; i++) {
+        if (sArray[i] == '(') {
+            stack.push(i); // mark for potential deletion
+        } else if (sArray[i] == ')') {
+            if (stack.length == 0) {
+                sArray[i] = ''; // delete the closing one
             } else {
-                continue; // 잘못된 괄호 제거
+                stack.pop() // found a match
             }
         }
-        str += char;
     }
 
-    // 남은 열린 괄호 제거
-    while (stack.length > 0) {
-        str = sliceStr(str);
-        stack.pop();
+    // anything left in the stack is unmatched openings. delete them
+    for (let i = 0; i < stack.length; i++) {
+        sArray[stack[i]] = '';
     }
 
-    return str;
+    return sArray.join('');
 };
 
-// 괄호를 제거하는 함수
-var sliceStr = function(str) {
-    let slice = '';
-    for (let i = str.length - 1; i >= 0; i--) {
-        if (str[i] === '(') {
-            slice = str.slice(0, i) + str.slice(i + 1);
-            break;
-        }
-    }
-    return slice;
-};
+// // 괄호를 제거하는 함수
+// var sliceStr = function(str) {
+//     let slice = '';
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         if (str[i] === '(') {
+//             slice = str.slice(0, i) + str.slice(i + 1);
+//             break;
+//         }
+//     }
+//     return slice;
+// };
