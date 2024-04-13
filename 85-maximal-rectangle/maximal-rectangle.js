87967cf9dbde388ffe060f683b4c44c1986bb17f
JeonB <newjeonb@gmail.com>
2024-04-13 19:33:14 +0900

    Time: 59 ms (98.46%) | Memory: 52.5 MB (80.86%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.js
+++ 85-maximal-rectangle/maximal-rectangle.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var maximalRectangle = function(matrix) {
-     if (matrix === null || matrix.length === 0 || matrix[0] === null || matrix[0].length === 0) return 0;
+  
     let m = matrix.length, n = matrix[0].length, maxArea = 0;
     let left = new Array(n).fill(0);
     let right = new Array(n).fill(n - 1);
