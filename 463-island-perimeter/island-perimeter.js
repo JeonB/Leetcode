47b561992cf6b91b55fbb2932a5eca30e3c8aeb2
JeonB <newjeonb@gmail.com>
2024-04-18 14:33:47 +0900

    Time: 705 ms (5.17%) | Memory: 61.5 MB (5.57%) - LeetSync

---
+++ 463-island-perimeter/island-perimeter.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var islandPerimeter = function(grid) {
+    let sum = 0
+
+    for(let i = 0 ; i < grid.length ; i++){
+            for(let j = 0 ; j < grid[i].length ; j++){
+                if(grid[i][j] === 1){
+                    sum += 4
+                    if(j < grid[i].length -1 && grid[i][j+1] === 1)
+                        sum -= 1
+                    if(j > 0  && grid[i][j-1] === 1)
+                        sum -= 1
+                    if(i < grid.length -1  && grid[i+1][j] === 1)
+                        sum -= 1   
+                    if(i > 0 && grid[i-1][j] === 1)
+                        sum -= 1
+                }
+                    
+                console.log(sum)                                                  
+        }
+    }
+    return sum
+   
+};
