72b186c4ab3835af914db6ce68c4689c9d6d0214
JeonB <newjeonb@gmail.com>
2024-04-18 14:35:02 +0900

    Time: 122 ms (81.91%) | Memory: 57.5 MB (80.12%) - LeetSync

--- 463-island-perimeter/island-perimeter.js
+++ 463-island-perimeter/island-perimeter.js
@@ -3,23 +3,25 @@
  * @return {number}
  */
 var islandPerimeter = function(grid) {
-    let sum = 0
-
-    for(let i = 0 ; i < grid.length ; i++){
-            for(let j = 0 ; j < grid[i].length ; j++){
-                if(grid[i][j] === 1){
-                    sum += 4
-                    if(j < grid[i].length -1 && grid[i][j+1] === 1)
-                        sum -= 1
-                    if(j > 0  && grid[i][j-1] === 1)
-                        sum -= 1
-                    if(i < grid.length -1  && grid[i+1][j] === 1)
-                        sum -= 1   
-                    if(i > 0 && grid[i-1][j] === 1)
-                        sum -= 1
-                }                                                
+    const rows = grid.length;
+    const cols = grid[0].length;
+    let perimeter = 0;
+    
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (grid[i][j] === 1) {
+                perimeter += 4;
+                
+                if (i > 0 && grid[i - 1][j] === 1) {
+                    perimeter -= 2;
+                }
+                
+                if (j > 0 && grid[i][j - 1] === 1) {
+                    perimeter -= 2;
+                }
+            }
         }
     }
-    return sum
-   
+    
+    return perimeter;
 };
