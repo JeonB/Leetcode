ce9bb78381558e96cebb7d641ef81c6f7ebe5f45
JeonB <newjeonb@gmail.com>
2024-05-29 23:19:37 +0900

    Time: 59 ms (29.63%) | Memory: 49.2 MB (48.15%) - LeetSync

---
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numSteps = function(s) {
+     let steps = 0, carry = 0;
+    for(let i=s.length-1;i>0;i--){
+        if(s.charAt(i) - '0' +carry == 1){
+            carry = 1;
+            steps += 2;
+        }
+        else{
+            steps++;
+        }
+    }
+    return carry + steps;
+};
