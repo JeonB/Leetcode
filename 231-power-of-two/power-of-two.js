5944f1f8bcdba12f418be815cb9767116e0ab935
JeonB <newjeonb@gmail.com>
2024-06-09 23:08:44 +0900

    Time: 82 ms (27.60%) | Memory: 52.2 MB (93.63%) - LeetSync

---
+++ 231-power-of-two/power-of-two.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    if(n<=0)
+        return false
+    if(n == 1)
+        return true
+    if( n%2 !== 0)
+        return false
+
+    n = isPowerOfTwo(n/2)
+    return n
+};
