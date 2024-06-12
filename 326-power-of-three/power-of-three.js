c834dee67efd29f3b683f4870d5ce8e3b214ab9f
JeonB <newjeonb@gmail.com>
2024-06-12 23:10:36 +0900

    Time: 166 ms (34.05%) | Memory: 58.4 MB (77.62%) - LeetSync

--- 326-power-of-three/power-of-three.js
+++ 326-power-of-three/power-of-three.js
@@ -3,9 +3,6 @@
  * @return {boolean}
  */
 var isPowerOfThree = function(n) {
-    if( n > 2 &&n%3 !== 0)return false
-    if( n<=0 )return false
-    if( n === 1) return true
-    return isPowerOfThree(n/3)
-   
+    const maxPowerOfThree = Math.pow(3,Math.round( Math.log(0x7fffffff) / Math.log(3)));
+    return n>0 && maxPowerOfThree%n==0;
 };
