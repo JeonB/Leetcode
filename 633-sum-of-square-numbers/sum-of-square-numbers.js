7d257cd7a65535ac5022080c0342a02e504a3e27
JeonB <newjeonb@gmail.com>
2024-06-17 19:49:47 +0900

    Time: 56 ms (72.60%) | Memory: 49.2 MB (83.56%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} c
+ * @return {boolean}
+ */
+var judgeSquareSum = function(c) {
+    let i = 0;
+    let j = Math.ceil(Math.sqrt(c));
+    while(i<=j){
+        let sum = i*i + j*j;
+        if(sum < c){
+            i++;
+        }else if (sum > c){
+            j--;
+        }else{
+            return true;
+        }
+    }
+    return false;
+};
