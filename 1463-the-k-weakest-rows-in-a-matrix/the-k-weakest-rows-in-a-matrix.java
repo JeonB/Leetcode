d84a887c68e7032ac142563ce8cc69a2146f96cc
JeonB <newjeonb@gmail.com>
2024-07-12 09:41:15 +0900

    Time: 2 ms (83.37%) | Memory: 44.6 MB (64.50%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
@@ -4,7 +4,7 @@ class Solution {
         int n = mat[0].length;
         int[] soldierCount = new int[m];
         
-        // 각 행의 병사 수를 계산합니다.
+        
         for (int i = 0; i < m; i++) {
             int count = 0;
             for (int j = 0; j < n; j++) {
@@ -17,16 +17,14 @@ class Solution {
             soldierCount[i] = count;
         }
         
-        // 우선순위 큐를 사용하여 행을 병사 수와 행 번호로 저장합니다.
         PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a, b) -> a[0] != b[0] ? Integer.compare(a[0], b[0]) : Integer.compare(a[1], b[1])
+            (soldiersA, soldiersB) -> soldiersA[0] != soldiersB[0] ? Integer.compare(soldiersA[0], soldiersB[0]) : Integer.compare(soldiersA[1], soldiersB[1])
         );
         
         for (int i = 0; i < m; i++) {
             pq.offer(new int[]{soldierCount[i], i});
         }
         
-        // 결과 배열에 가장 약한 k개의 행의 인덱스를 저장합니다.
         int[] result = new int[k];
         for (int i = 0; i < k; i++) {
             result[i] = pq.poll()[1];
