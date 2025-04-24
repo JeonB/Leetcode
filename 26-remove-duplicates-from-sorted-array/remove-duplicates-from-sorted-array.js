95b57e82d8b3bd7d832807cb6b0775c5653bdf4a
JeonB <newjeonb@gmail.com>
2025-04-24 23:29:47 +0900

    Time: 0 ms (100.00%) | Memory: 58.5 MB (14.65%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,12 @@
+var removeDuplicates = function(nums) {
+    let i = 1;
+
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[j] !== nums[i - 1]) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+
+    return i;    
+};
